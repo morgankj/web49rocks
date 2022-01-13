4492ef1f81aa96128f5457fdfaec871f488487f2
morgankj <morgan.kjones95@gmail.com>
2022-01-13 12:10:51 -0600

    work in progress

---
+++ index.js
@@ -0,0 +1,24 @@
+const cohort1 = process.argv[2]
+const user = process.env.USER
+const shell = process.env.SHELL
+
+if (cohort1 === 'web-49') {
+  console.log(`${cohort1} is the best forever!`)
+} else {
+  console.log(`booooh cohort ${cohort1}`)
+}
+
+console.log(`the user is ${user}`)
+console.log(`the shell is ${shell}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'hey there'})
+})
+
+const port = 9000
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
